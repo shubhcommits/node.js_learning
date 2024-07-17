fc561df9d765ee2a6f81a584e260d183cdbfc85f
Shubham <shm.code1@gmail.com>
2024-07-17 23:11:58 +0530

    refactor_commandwatch using event emitter

--- file_system/command_watch/app.js
+++ file_system/command_watch/app.js
@@ -3,22 +3,29 @@ const fs=require("fs/promises");
 (async()=>{
   // opening file
   const commanFileHandler=await fs.open("./command.txt","r");  // opened in read mode
+  commanFileHandler.on("change", async()=>{
+    console.log("file was changed");
+
+    // reading content of the file
+
+    // console.log(await commanFileHandler.stat());
+    // allocate the buffer with the size of the file
+    const size=(await commanFileHandler.stat()).size;  // stat is containing some stats of the file
+    const buff=Buffer.alloc(size);  //allocating buffer with the size of the file
+    // await commanFileHandler/stat()
+    const offSet=0;  // location we want to start filling our buffer
+    const length=buff.byteLength; // how many bytes we want to read
+    const position=0;  // position we want to start reading
+
+    // reding whole content from begining to the end
+    const content=await commanFileHandler.read(buff,offSet,length,position);
+    console.log(content);
+    console.log(buff.toString("utf-8"));
+  })
   const watcher=fs.watch("./");// watching whole directory
   for await (const event of watcher){
     if(event.eventType==="change"&&event.filename==="command.txt"){
-      console.log("file was changed");
-
-      // reading content of the file
-
-      // console.log(await commanFileHandler.stat());
-      const size=(await commanFileHandler.stat()).size;
-      const buff=Buffer.alloc(size);  
-      // await commanFileHandler/stat()
-      const offSet=0;
-      const length=buff.byteLength;
-      const position=0;
-      const content=await commanFileHandler.read(buff,offSet,length,position);
-      console.log(content);
+      commanFileHandler.emit("change");
     }
     // console.log(event);
   }
